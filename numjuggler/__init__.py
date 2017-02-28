f4b695623959cd71c08dfc4e55fc63408853f097
travleev <anton.travleev@kit.edu>
2017-02-28 12:51:33 +0100

    Trivial module to test import from brython

--- numjuggler/__init__.py
+++ numjuggler/__init__.py
@@ -5,3 +5,4 @@ Package provides script mcnp.juggler. See its help for description.
 
 """
 
+from .parser2 import f
---
+++ numjuggler/parser2.py
@@ -0,0 +1,7 @@
+"""
+This is a module to test importing.
+"""
+
+
+def f():
+    return 'Hello from parser2'
